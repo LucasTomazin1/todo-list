45e8264373987bd942bb26f5a7cd438690e92cae
Lucas Tomazini <lucasgtomazini@gmail.com>
2024-08-29 12:06:27 -0300

    function and style adjustments

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -3,11 +3,11 @@ import { TodoManager } from '../TodoManager'
 
 export const Board: React.FC<BoardInterface> = ({ tittle, isTodo, isNote }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col max-w-[300px]">
       <header>
-        <h1>{tittle}</h1>
+        <h1 className="text-xl text-bold">{tittle}</h1>
       </header>
-      <div>
+      <div className="flex border border-zinc-400 rounded-md p-2 text-zinc-300">
         {isNote && <p>notanotaotnaotnaotna</p>}
         {isTodo && <TodoManager />}
       </div>
--- src/components/TodoList/components/NewTodoItem/index.tsx
+++ src/components/TodoList/components/NewTodoItem/index.tsx
@@ -31,8 +31,9 @@ export const NewTodoItem: React.FC<NewTodoItemProps> = ({ addTodo }) => {
 
   return (
     <>
-      <div className="p-2">
+      <div className="flex m-2 rounded-2xl overflow-hidden border border-zinc-400 bg-gray-800">
         <input
+          className="bg-gray-800 p-1 pl-2"
           placeholder="Nova Tarefa"
           type="text"
           onChange={onChangeHandler}
@@ -41,7 +42,7 @@ export const NewTodoItem: React.FC<NewTodoItemProps> = ({ addTodo }) => {
           onKeyPress={onEnterPress}
         />
         <button
-          className="border-2 border-black rounded-md px-2 cursor-pointer"
+          className="py-1 px-5 cursor-pointer border-l border-zinc-400"
           onClick={onClickHandler}
         >
           <FaPlus />
